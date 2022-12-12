6e242fef3d1329753ba7909b1229d2b1f130e53d
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-12 14:59:11 -0300

    Agregar entidad Turnos

---
+++ SistemaTurnosOnline.Models/Turnos.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace SistemaTurnosOnline.Models
+{
+    public class Turnos
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+        public int OrdenEnCola { get; set; }
+    }
+}
