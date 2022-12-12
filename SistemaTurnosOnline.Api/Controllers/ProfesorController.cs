3dceaf5567e4724e68ce82aca0b85c226f61067e
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-12 14:32:45 -0300

    Agregar controladores

---
+++ SistemaTurnosOnline.Api/Controllers/ProfesorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaTurnosOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+    }
+}
