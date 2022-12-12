189dc7283fd8ce712dd2a710dfa6c0c351ecb7c1
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-12 14:58:59 -0300

    Eliminar Legajo. Agregar Dni, email, estado y password

--- SistemaTurnosOnline.Models/Profesor.cs
+++ SistemaTurnosOnline.Models/Profesor.cs
@@ -13,8 +13,11 @@ namespace SistemaTurnosOnline.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        public string Dni { get; set; }
         public string Nombre { get; set; }
-        public string Legajo { get; set; }
+        public string Email { get; set; }
+        public bool Estado { get; set; }
+        public string Password { get; set; }
         public List<Carrera> Carreras { get; set; }
     }
 }
