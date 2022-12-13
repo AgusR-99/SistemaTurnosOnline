33aba0f6d9f3ee2bac7841d39bffacbf5b40e946
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-13 19:41:16 -0300

    Implementar metodos GetProfesores, GetProfesor y CreateProfesor

--- SistemaTurnosOnline.Api/Controllers/ProfesorController.cs
+++ SistemaTurnosOnline.Api/Controllers/ProfesorController.cs
@@ -7,5 +7,74 @@ namespace SistemaTurnosOnline.Api.Controllers
     [ApiController]
     public class ProfesorController : ControllerBase
     {
+        private readonly IProfesorRepository profesorRepository;
+
+        public ProfesorController(IProfesorRepository profesorRepository)
+        {
+            this.profesorRepository = profesorRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProfesores()
+        {
+            try
+            {
+                var profesores = await profesorRepository.GetProfesores();
+
+                if (profesores == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(profesores);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProfesor(string id)
+        {
+            try
+            {
+                var profesor = await profesorRepository.GetProfesor(id);
+                
+                if (profesor == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(profesor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProfesor([FromBody] Profesor profesor)
+        {
+            try
+            {
+                var newProfesor = await profesorRepository.CreateProfesor(profesor);
+
+                if (newProfesor == null)
+                {
+                    return NoContent();
+                }
+
+                return CreatedAtAction(nameof(GetProfesor), new {id = newProfesor.Id}, newProfesor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
     }
 }
