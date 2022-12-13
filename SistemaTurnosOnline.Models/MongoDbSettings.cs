c3414696480c2206197de4c6c7dc62a3307fa6b3
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-13 19:30:39 -0300

    Agregar modelo para opciones de MongoDb

---
+++ SistemaTurnosOnline.Models/MongoDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTurnosOnline.Models
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionURI { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+    }
+}
