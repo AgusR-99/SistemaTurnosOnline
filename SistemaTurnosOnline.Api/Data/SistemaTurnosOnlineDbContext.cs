57fa8787a1457fe74b1d799c20b681bd973ac1c6
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-13 14:43:28 -0300

    Agregar mongoDb Context

---
+++ SistemaTurnosOnline.Api/Data/SistemaTurnosOnlineDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace SistemaTurnosOnline.Api.Data
+{
+    public class SistemaTurnosOnlineDbContext
+    {
+        private readonly IConfiguration configuration;
+        public MongoClient? client;
+        public IMongoDatabase db;
+        public SistemaTurnosOnlineDbContext(IConfiguration configuration)
+        {
+            // Se accede al connection string
+            var connectionString = this.configuration["CONNECTION_STRING"];
+
+            // Se inicializa una nueva instancia de MongoCliente mediante el connection string
+            var client = new MongoClient(connectionString);
+
+            db = client.GetDatabase("turnosdb");
+        }
+    }
+}
