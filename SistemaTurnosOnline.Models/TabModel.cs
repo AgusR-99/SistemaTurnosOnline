410236a5c868c8971a08087fcd7127be62dbc6a3
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-15 00:46:37 -0300

    Agregar modelo de Tab

---
+++ SistemaTurnosOnline.Models/TabModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTurnosOnline.Models
+{
+    public class TabModel
+    {
+        public string Class { get; set; }
+        public string Text { get; set; }
+    }
+}
