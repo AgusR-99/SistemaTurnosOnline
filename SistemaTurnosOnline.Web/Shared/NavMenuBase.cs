120a3290849e8c1d9e09666d7173ea92b10e1465
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-15 00:50:23 -0300

    Agregar codigo de NavMenu

---
+++ SistemaTurnosOnline.Web/Shared/NavMenuBase.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+using SistemaTurnosOnline.Models;
+
+namespace SistemaTurnosOnline.Web.Shared
+{
+    public class NavMenuBase : ComponentBase
+    {
+        protected bool collapseNavMenu = true;
+
+        protected string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        protected void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+        public List<TabModel> tabsProfesor { get; set; }
+        public List<TabModel> tabsCarreras { get; set; }
+
+        public NavMenuBase()
+        {
+            tabsProfesor = new List<TabModel>()
+            {
+                new TabModel()
+                {
+                    Text = "Agregar",
+                    Class = "oi oi-plus"
+                },
+                new TabModel()
+                {
+                    Text = "Listar todos",
+                    Class = "oi oi-grid-four-up"
+                },
+                new TabModel()
+                {
+                    Text = "Buscar",
+                    Class = "oi oi-magnifying-glass"
+                },
+                new TabModel()
+                {
+                    Text = "Actualizar",
+                    Class = "oi oi-loop-circular"
+                },
+                new TabModel()
+                {
+                    Text = "Eliminar",
+                    Class = "oi oi-trash"
+                },
+            };
+
+            tabsCarreras = new List<TabModel>()
+            {
+                new TabModel()
+                {
+                    Text = "Agregar",
+                    Class = "oi oi-plus"
+                },
+                new TabModel()
+                {
+                    Text = "Listar todos",
+                    Class = "oi oi-grid-four-up"
+                },
+                new TabModel()
+                {
+                    Text = "Buscar",
+                    Class = "oi oi-magnifying-glass"
+                },
+                new TabModel()
+                {
+                    Text = "Eliminar",
+                    Class = "oi oi-trash"
+                },
+            };
+        }
+    }
+}
