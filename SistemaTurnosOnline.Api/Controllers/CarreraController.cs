546fb21bb8a0f8eec72e3c719a6dc88a427a6054
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 03:08:21 -0300

    Definir e implementar metodos CR

--- SistemaTurnosOnline.Api/Controllers/CarreraController.cs
+++ SistemaTurnosOnline.Api/Controllers/CarreraController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SistemaTurnosOnline.Api.Repositories;
+using SistemaTurnosOnline.Api.Repositories.Contracts;
+using SistemaTurnosOnline.Models;
 
 namespace SistemaTurnosOnline.Api.Controllers
 {
@@ -7,5 +10,74 @@ namespace SistemaTurnosOnline.Api.Controllers
     [ApiController]
     public class CarreraController : ControllerBase
     {
+        private readonly ICarreraRepository carreraRepository;
+
+        public CarreraController(ICarreraRepository carreraRepository)
+        {
+            this.carreraRepository = carreraRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCarreras()
+        {
+            try
+            {
+                var carreras = await carreraRepository.GetCarreras();
+
+                if (carreras == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(carreras);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCarrera(string id)
+        {
+            try
+            {
+                var carrera = await carreraRepository.GetCarrera(id);
+
+                if (carrera == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(carrera);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCarrera([FromBody] Carrera carrera)
+        {
+            try
+            {
+                var newCarrera = await carreraRepository.CreateCarrera(carrera);
+
+                if (newCarrera == null)
+                {
+                    return NoContent();
+                }
+
+                return CreatedAtAction(nameof(GetCarrera), new { id = newCarrera.Id }, newCarrera);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
     }
 }
