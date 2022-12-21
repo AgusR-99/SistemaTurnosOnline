1c4100f55d38209a1a76c6f6326d336f9cc3fe32
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 03:11:41 -0300

    Agregar clase con enums de atributos de formulario de profesor

---
+++ SistemaTurnosOnline.Models/AttributeCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTurnosOnline.Models
+{
+    public class AttributeCheck
+    {
+        public enum Attribute
+        {
+            Dni,
+            Email,
+            Nombre,
+            Password,
+            PasswordConfirm
+        }
+    }
+}
