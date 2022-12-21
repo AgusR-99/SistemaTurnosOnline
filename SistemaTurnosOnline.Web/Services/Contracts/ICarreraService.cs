1c45833f47cfd1a1ed4cb11f89e7ac28d850fb68
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 03:13:21 -0300

    Agregar servicio de Carrera

---
+++ SistemaTurnosOnline.Web/Services/Contracts/ICarreraService.cs
@@ -0,0 +1,15 @@
+﻿using SistemaTurnosOnline.Models;
+
+namespace SistemaTurnosOnline.Web.Services.Contracts
+{
+    public interface ICarreraService
+    {
+        Task<List<Carrera>> GetCarreras();
+        List<string> GetCarrerasValues();
+        void SetCarrerasValues(List<string> carreras);
+        Task<Carrera> GetCarrera(string id);
+        Task<Carrera> CreateCarrera(Carrera carrera);
+        Task<Carrera> UpdateCarrera(Carrera carrera);
+        Task<Carrera> DeleteCarrera(string id);
+    }
+}
