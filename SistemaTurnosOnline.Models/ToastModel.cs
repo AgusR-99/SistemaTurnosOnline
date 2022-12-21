2f2fb8f616daf61cd150ffc2d74555d582bde986
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 20:21:40 -0300

    Agregar modelo para Toast de bootstrap

---
+++ SistemaTurnosOnline.Models/ToastModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SistemaTurnosOnline.Web.Pages
+{
+    public class ToastModel
+    {
+        public enum Status
+        {
+            Success,
+            Error,
+            Pending
+        }
+        public Status status { get; set; }
+        public string Id { get; set; }
+        public string Class { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Time { get; set; }
+        public string Text { get; set; }
+        public ToastModel(Status status, string id, string @class, string icon, string title, string time, string text)
+        {
+            this.status = status;
+            Id = id;
+            Icon = icon;
+            Class = @class;
+            Title = title;
+            Time = time;
+            Text = text;
+        }
+    }
+}
