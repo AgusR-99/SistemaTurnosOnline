108c6df0d317cd3755b6e6c11cf9fd26449ace2c
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 03:10:04 -0300

    Agregar CustomRouteConstraint

---
+++ SistemaTurnosOnline.Api/Extensions/CustomRouteConstraint.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaTurnosOnline.Api.Extensions
+{
+    public class CustomRouteConstraint<TEnum> : IRouteConstraint
+             where TEnum : struct, Enum
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            // retrieve the candidate value
+            var candidate = values[routeKey]?.ToString();
+            // attempt to parse the candidate to the required Enum type, and return the result
+            return Enum.TryParse(candidate, true, out TEnum _);
+        }
+    }
+}
