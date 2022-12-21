f4777d4f224c46637f7669977f47d9a1efca0841
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 20:27:01 -0300

    Injectar IJSRuntime y agregar funcionalidad para Toast

--- SistemaTurnosOnline.Web/Pages/CrearProfesorBase.cs
+++ SistemaTurnosOnline.Web/Pages/CrearProfesorBase.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.JSInterop;
 using SistemaTurnosOnline.Models;
 using SistemaTurnosOnline.Web.Services.Contracts;
-using System.ComponentModel.DataAnnotations;
 
 namespace SistemaTurnosOnline.Web.Pages
 {
     public class CrearProfesorBase : ComponentBase
     {
+        [Inject]
+        public IJSRuntime Js { get; set; }
         [Inject]
         public IProfesorService ProfesorService { get; set; }
         [Inject]
@@ -15,7 +16,27 @@ namespace SistemaTurnosOnline.Web.Pages
         public ProfesorForm ProfesorForm { get; set; } = new ProfesorForm();
         public string ErrorMessage { get; set; }
         public List<Carrera> Carreras { get; set; }
-
+        public List<ToastModel> Toasts { get; set; } = new List<ToastModel>
+        {
+              new ToastModel(
+                status: ToastModel.Status.Success,
+                id: "toastCreado",
+                @class: "toast text-bg-success",
+                icon: "oi oi-circle-check",
+                title: "Solicitud exitosa!",
+                time: "Ahora",
+                text: "Se ha enviado la solicitud de alta al administrador"
+                ),
+             new ToastModel(
+                status: ToastModel.Status.Error,
+                id: "toastError",
+                @class: "toast text-bg-danger",
+                icon: "oi oi-circle-x",
+                title: "Error de server",
+                time: "Ahora",
+                text: "Se ha producido un error al enviar la solicitud"
+                )
+        };
         protected override async Task OnInitializedAsync()
         {
             try
@@ -28,6 +49,11 @@ namespace SistemaTurnosOnline.Web.Pages
 
                 throw;
             }
+        }
+
+        protected async Task ShowToast(string id)
+        {
+            await Js.InvokeVoidAsync(identifier: "showToast", id);
         }
 
         protected void Checkbox_Click(string id)
@@ -78,6 +104,29 @@ namespace SistemaTurnosOnline.Web.Pages
                 profesor.CarrerasId = carrerasId.ToList();
 
                 var profesorToAdd = await ProfesorService.CreateProfesor(profesor);
+
+                if (profesorToAdd != null)
+                {
+                    var toast = Toasts.Find(t => t.status == ToastModel.Status.Success);
+
+                    if (toast != null)
+                    {
+                        await ShowToast(toast.Id);
+                    }
+                    else throw new NullReferenceException($"No se ha encontrado {nameof(ToastModel)} con {nameof(ToastModel.Status.Success)}" +
+                        $"asegurese que dicho parametro se encuentre presente en la lista");
+                }
+                else
+                {
+                    var toast = Toasts.Find(t => t.status == ToastModel.Status.Error);
+
+                    if (toast != null)
+                    {
+                        await ShowToast(toast.Id);
+                    }
+                    else throw new NullReferenceException($"No se ha encontrado {nameof(ToastModel)} con {nameof(ToastModel.Status.Error)}:" +
+                        $"asegurese que dicho parametro se encuentre presente en la lista");
+                }
             }
             catch (Exception ex)
             {
