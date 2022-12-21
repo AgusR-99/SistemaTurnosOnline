b10c229e24a505395a9cb58169bbb71de1362271
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-21 03:06:40 -0300

    Modificar retorno de metodos CUD

--- SistemaTurnosOnline.Api/Repositories/Contracts/ICarreraRepository.cs
+++ SistemaTurnosOnline.Api/Repositories/Contracts/ICarreraRepository.cs
@@ -4,9 +4,9 @@ namespace SistemaTurnosOnline.Api.Repositories.Contracts
 {
     public interface ICarreraRepository
     {
-        Task CreateCarrera(Carrera professor);
-        Task UpdateCarrera(Carrera professor);
-        Task DeleteCarrera(string id);
+        Task<Carrera> CreateCarrera(Carrera professor);
+        Task<Carrera> UpdateCarrera(Carrera professor);
+        Task<Carrera> DeleteCarrera(string id);
         Task<List<Carrera>> GetCarreras();
         Task<Carrera> GetCarrera(string id);
     }
