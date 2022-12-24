bb65a96429e3ab441e39ac413bd3a7b48902d76f
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-24 19:28:11 -0300

    Agregar modelo de formulario de profesor

---
+++ SistemaTurnosOnline.Models/ProfesorForm.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaTurnosOnline.Models
+{
+    public class ProfesorForm
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public List<string>? CarrerasId { get; set; }
+    }
+}
