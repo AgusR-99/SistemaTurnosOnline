c6f6a9a8faa04a96438164469cc68e8a70b4278f
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-24 20:30:11 -0300

    Agregar servicio de validador de profesor

---
+++ SistemaTurnosOnline.Web/Services/ValidateProfesorService.cs
@@ -0,0 +1,63 @@
+﻿using SistemaTurnosOnline.Models;
+using SistemaTurnosOnline.Models.Validators.Contracts;
+using System.Linq.Expressions;
+using System.Net;
+
+namespace SistemaTurnosOnline.Web.Services
+{
+    public class ValidateProfesorService : IValidateProfesor
+    {
+        private readonly HttpClient httpClient;
+
+        public ValidateProfesorService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        private async Task<bool> responseStatus (HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return false;
+            }
+            else if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return true;
+            }
+            else
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                throw new Exception(message);
+            }
+        }
+
+        public async Task<bool> ValidateDni(string dni)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"api/Profesor/GetByDni/{dni}");
+
+                return await responseStatus(response);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ValidateEmail(string email)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"api/Profesor/GetByEmail/{email}");
+
+                return await responseStatus(response);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
