20dd5aefe458390e372649cba30e61b2999502c7
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-24 19:27:06 -0300

    Agregar interfaz validador de profesor

---
+++ SistemaTurnosOnline.Models/Validators/Contracts/IValidateProfesorParams.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace SistemaTurnosOnline.Models.Validators.Contracts
+{
+    public interface IValidateProfesor
+    {
+        Task<bool> ValidateEmail(string email);
+        Task<bool> ValidateDni(string dni);
+    }
+}
