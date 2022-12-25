3766b23e71f4cbb6cd465ef81f5647b5aa582d25
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-24 23:35:32 -0300

    Agregar implementacion de validador de profesor

---
+++ SistemaTurnosOnline.Api/Validator/ValidateProfesor.cs
@@ -0,0 +1,43 @@
+﻿using SistemaTurnosOnline.Api.Repositories.Contracts;
+using SistemaTurnosOnline.Models;
+using SistemaTurnosOnline.Models.Validators.Contracts;
+using System.Linq.Expressions;
+
+namespace SistemaTurnosOnline.Api.Validator
+{
+    public class ValidateProfesor : IValidateProfesor
+    {
+        private readonly IProfesorRepository profesorRepository;
+
+        public ValidateProfesor(IProfesorRepository profesorRepository)
+        {
+            this.profesorRepository = profesorRepository;
+        }
+
+        public async Task<bool> ValidateDni(string dni)
+        {
+            Expression<Func<Profesor, string>> field = p => p.Dni;
+            var profesor = await profesorRepository.GetProfesorByParam(dni, field);
+
+            if (profesor == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> ValidateEmail(string email)
+        {
+            Expression<Func<Profesor, string>> field = p => p.Email;
+            var profesor = await profesorRepository.GetProfesorByParam(email, field);
+
+            if (profesor == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
