8b507d43409b4a12197e22eeef26294334986780
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-25 02:24:16 -0300

    Agregar parametro HeaderClass

--- SistemaTurnosOnline.Models/ToastModel.cs
+++ SistemaTurnosOnline.Models/ToastModel.cs
@@ -10,16 +10,18 @@
         }
         public Status status { get; set; }
         public string Id { get; set; }
+        public string HeaderClass { get; set; }
         public string Class { get; set; }
         public string Icon { get; set; }
         public string Title { get; set; }
         public string Time { get; set; }
         public string Text { get; set; }
-        public ToastModel(Status status, string id, string @class, string icon, string title, string time, string text)
+        public ToastModel(Status status, string headerClass, string id, string @class, string icon, string title, string time, string text)
         {
             this.status = status;
             Id = id;
             Icon = icon;
+            HeaderClass = headerClass;
             Class = @class;
             Title = title;
             Time = time;
