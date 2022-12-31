ab6991b63c73f4aa2f577620358ecca977361af5
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:24:39 -0300

    Agregar miembro Id

--- SistemaTurnosOnline.Models/ProfesorForm.cs
+++ SistemaTurnosOnline.Models/ProfesorForm.cs
@@ -2,6 +2,7 @@
 {
     public class ProfesorForm
     {
+        public string? Id { get; set; }
         public string Dni { get; set; }
         public string Nombre { get; set; }
         public string Email { get; set; }
