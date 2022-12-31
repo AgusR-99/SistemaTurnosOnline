c0dc707045cc8a3d6650d7bb86cca8cf0c86fbff
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:46:52 -0300

    Agregar metodo showPassword

---
+++ SistemaTurnosOnline.Web/wwwroot/js/showpassword.js
@@ -0,0 +1,8 @@
+﻿function showPassword(id) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
