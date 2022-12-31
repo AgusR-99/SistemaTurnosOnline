21d8fe98815d9a3b6b7f417bff26a42065abfd7c
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:55:11 -0300

    Agregar funcionalidad Listar Profesores

---
+++ SistemaTurnosOnline.Web/Pages/ListarProfesores/ListarTodosProfesorBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SistemaTurnosOnline.Models;
+using SistemaTurnosOnline.Web.Services.Contracts;
+
+namespace SistemaTurnosOnline.Web.Pages.ListarProfesores
+{
+    public class ListarTodosProfesorBase : ComponentBase, IDisposable
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+        [Inject]
+        public IProfesorService ProfesorService { get; set; }
+        public IEnumerable<Profesor> Profesores { get; set; }
+        public List<string> Headers { get; set; } = new List<string> { "DNI", "Nombre", "Email", "" };
+        [Parameter]
+        public string TableId { get; set; } = "profesoresTable";
+        protected override async Task OnInitializedAsync()
+        {
+            Profesores = await ProfesorService.GetProfesores();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            {
+                await Js.InvokeAsync<object>(identifier: "datatableInit", "#" + TableId);
+                await base.OnAfterRenderAsync(firstRender);
+            }
+        }
+
+        //https://datatables.net/forums/discussion/56389/datatables-with-blazor
+        public async void Dispose()
+        {
+            await Js.InvokeAsync<object>(identifier: "datatableRemove", "#" + TableId);
+        }
+    }
+}
