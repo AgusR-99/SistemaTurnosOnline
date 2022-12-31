2b4b4027562247fe65eeb3113381b4700c11ce2d
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:27:16 -0300

    Actualizar metodo Update

--- SistemaTurnosOnline.Api/Repositories/Contracts/IProfesorRepository.cs
+++ SistemaTurnosOnline.Api/Repositories/Contracts/IProfesorRepository.cs
@@ -6,7 +6,7 @@ namespace SistemaTurnosOnline.Api.Repositories.Contracts
     public interface IProfesorRepository
     {
         Task<Profesor> CreateProfesor(Profesor profesor);
-        Task<Profesor> UpdateProfesor(Profesor profesor);
+        Task<Profesor> UpdateProfesor(Profesor profesor, string id);
         Task<Profesor> DeleteProfesor(string id);
         Task<Profesor> GetProfesorByParam(string value, Expression<Func<Profesor, string>> field);
         Task<Profesor> GetProfesor(string id);
