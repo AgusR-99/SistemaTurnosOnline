c177b60f5f8e7b45a12ce00ad5db4871549e3c34
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:20:26 -0300

    Mover Extensions a .Shared. Agregar  metodos ConvertToCarrera y ConvertToCarreraForm

---
+++ SistemaTurnosOnline.Models/Extensions/Conversions.cs
@@ -0,0 +1,52 @@
+﻿using SistemaTurnosOnline.Models;
+using SistemaTurnosOnline.Shared;
+
+namespace SistemaTurnosOnline.Api.Extensions
+{
+    public static class Conversions
+    {
+        public static Profesor ConvertToProfesor(this ProfesorForm profesorForm)
+        {
+            return new Profesor
+            {
+                Id = profesorForm.Id,
+                Dni = profesorForm.Dni,
+                Email = profesorForm.Email,
+                Nombre = profesorForm.Nombre,
+                Password = profesorForm.Password,
+                CarrerasId = profesorForm.CarrerasId
+            };
+        }
+
+        public static ProfesorForm ConvertToProfesorForm(this Profesor profesor)
+        {
+            return new ProfesorForm
+            {
+                Id = profesor.Id,
+                Dni = profesor.Dni,
+                Email = profesor.Email,
+                Nombre = profesor.Nombre,
+                Password = profesor.Password,
+                CarrerasId = profesor.CarrerasId
+            };
+        }
+
+        public static Carrera ConvertToCarrera(this CarreraForm carreraForm)
+        {
+            return new Carrera
+            {
+                Id = carreraForm.Id,
+                Nombre = carreraForm.Nombre
+            };
+        }
+
+        public static CarreraForm ConvertToCarreraForm(this Carrera carrera)
+        {
+            return new CarreraForm
+            {
+                Id = carrera.Id,
+                Nombre = carrera.Nombre
+            };
+        }
+    }
+}
