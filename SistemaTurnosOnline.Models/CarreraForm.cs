3286e7b43d960aa80a569a930c66f9b1b5f4bcdb
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:20:53 -0300

    Agregar modelo CarreraForm

---
+++ SistemaTurnosOnline.Models/CarreraForm.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaTurnosOnline.Shared
+{
+    public class CarreraForm
+    {
+        public string? Id { get; set; }
+        public string Nombre { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
