892e1dd526780b0c6a805f53dd8bd27d4000d185
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:46:34 -0300

    Agregar metodos datatableInit y datatableRemove

---
+++ SistemaTurnosOnline.Web/wwwroot/js/datatableInit.js
@@ -0,0 +1,14 @@
+﻿window.datatableInit = function datatableInit(table) {
+    $(document).ready(function () {
+        $(table).DataTable();
+    });
+}
+
+window.datatableRemove = function datatableRemove(table) {
+    $(document).ready(function () {
+        $(table).DataTable().destroy();
+    });
+
+    var elem = document.querySelector(table + '_wrapper');
+    elem.parentNode.removeChild(elem);
+}
