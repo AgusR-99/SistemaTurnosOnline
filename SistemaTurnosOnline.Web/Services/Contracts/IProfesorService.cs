9f508dfbdae4b5408cd96a5e3bdb811071f85ebe
Agustin Rodriguez <agus-socialclub@live.com>
2022-12-31 16:49:52 -0300

    Modificar GetProfesores y UpdateProfesor

--- SistemaTurnosOnline.Web/Services/Contracts/IProfesorService.cs
+++ SistemaTurnosOnline.Web/Services/Contracts/IProfesorService.cs
@@ -4,11 +4,11 @@ namespace SistemaTurnosOnline.Web.Services.Contracts
 {
     public interface IProfesorService
     {
-        Task<List<Profesor>> GetProfesores();
+        Task<IEnumerable<Profesor>> GetProfesores();
         Task<Profesor> GetProfesor(string id);
         Task<Profesor> CreateProfesor(ProfesorForm profesorForm);
         //TODO: cambiar a UpdateProfesor(ProfesorForm profesorForm);
-        Task<Profesor> UpdateProfesor(Profesor profesor);
+        Task<Profesor> UpdateProfesor(ProfesorForm profesorForm);
         Task DeleteProfesor(string id);
     }
 }
