28ddc9018fbf7c1ef2428130892d2af97da52eb2
Agustin Rodriguez <agus-socialclub@live.com>
2023-01-06 20:53:54 -0300

    Implementar listado de carreras

---
+++ SistemaTurnosOnline.Web/Pages/ListarCarreras/ListarCarrerasBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SistemaTurnosOnline.Models;
+using SistemaTurnosOnline.Web.Services.Contracts;
+
+namespace SistemaTurnosOnline.Web.Pages.ListarCarreras
+{
+    public class ListarCarrerasBase : ComponentBase, IDisposable
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+        [Inject]
+        public ICarreraService CarreraService { get; set; }
+        public IEnumerable<Carrera> Carreras { get; set; }
+        public List<string> Headers { get; set; } = new List<string> { "Nombre", "" };
+        [Parameter]
+        public string TableId { get; set; } = "carrerasTable";
+
+        protected async override Task OnInitializedAsync()
+        {
+            Carreras = await CarreraService.GetCarreras();
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            {
+                await Js.InvokeAsync<object>(identifier: "datatableInit", "#" + TableId);
+                await base.OnAfterRenderAsync(firstRender);
+            }
+        }
+
+        public async void Dispose()
+        {
+            await Js.InvokeAsync<object>(identifier: "datatableRemove", "#" + TableId);
+        }
+    }
+}
