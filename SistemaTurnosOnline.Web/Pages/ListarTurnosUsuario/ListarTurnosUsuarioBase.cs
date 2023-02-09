940fdc3d73e99e66c187369eb55402c2ee077ff0
Agustin Rodriguez <agus-socialclub@live.com>
2023-02-09 20:58:07 -0300

    Implementar finalizado de turno desde listado de turnos del usuario (#5)

--- SistemaTurnosOnline.Web/Pages/ListarTurnosUsuario/ListarTurnosUsuarioBase.cs
+++ SistemaTurnosOnline.Web/Pages/ListarTurnosUsuario/ListarTurnosUsuarioBase.cs
@@ -1,8 +1,9 @@
-﻿using Blazorise;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
+using SistemaTurnosOnline.Shared;
 using SistemaTurnosOnline.Shared.Turnos;
+using SistemaTurnosOnline.Web.Extensions;
 using SistemaTurnosOnline.Web.Services.Contracts;
 using System.Security.Claims;
 
@@ -16,15 +17,38 @@ namespace SistemaTurnosOnline.Web.Pages.ListarTurnosUsuario
         [Inject]
         public IJSRuntime Js { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [CascadingParameter]
         private Task<AuthenticationState> AuthenticationState { get; set; }
 
         [Parameter]
         public string TableId { get; set; } = "turnosTable";
 
-        public List<string> Headers { get; set; } = new List<string> { "Orden en cola", "Descripcion"};
+        public List<string> Headers { get; set; } = new List<string> { "Orden en cola", "Descripcion", ""};
 
         public IEnumerable<Turno> Turnos { get; set; }
+
+        public string FinalizarTurnoPromptModal { get; set; } = "FinishTaskPromptModal";
+
+        public string FinalizarTurnoExitoModal { get; set; } = "FinishTaskSuccessModal";
+
+        public ToastModel Toast { get; set; } =
+            new(
+                status: ToastModel.Status.Error,
+                id: "toastError",
+                headerClass: "bg-danger",
+                @class: "toast",
+                icon: "oi oi-circle-x",
+                title: "Error de server",
+                time: "Ahora",
+                text: "Se ha producido un error al enviar la solicitud"
+            );
+
+        public string TurnoId { get; set; }
+
+        public long Orden { get; set; }
 
         public long PosicionEnCola { get; set; }
 
@@ -39,6 +63,29 @@ namespace SistemaTurnosOnline.Web.Pages.ListarTurnosUsuario
             if (Turnos.Count() != 0) PosicionEnCola = Turnos.First().OrdenEnCola;
         }
 
+        protected async Task FinishTaskPrompt_Click(string turnoId, long orden)
+        {
+            TurnoId = turnoId;
+            Orden = orden;
+
+            await FinalizarTurnoPromptModal.ShowModal(Js);
+        }
+
+        protected async Task FinishTask_Click()
+        {
+            try
+            {
+                var deletedTurno = await TurnoService.DeleteTurno(TurnoId);
+
+                if (deletedTurno != null) await FinalizarTurnoExitoModal.ShowModal(Js);
+            }
+            catch (Exception ex)
+            {
+                Toast.Text = ex.Message;
+                await Toast.Id.ShowToast(Js);
+            }
+        }
+
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             {
@@ -51,6 +98,11 @@ namespace SistemaTurnosOnline.Web.Pages.ListarTurnosUsuario
         public async void Dispose()
         {
             await Js.InvokeAsync<object>(identifier: "datatableRemove", "#" + TableId);
+        }
+
+        protected void Navigate_Click()
+        {
+            NavigationManager.NavigateTo("turno/user-items", true);
         }
     }
 }
