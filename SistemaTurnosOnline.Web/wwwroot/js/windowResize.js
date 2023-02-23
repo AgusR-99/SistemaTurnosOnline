58b0d45ba23f0950327a4f414ed33b906b808cb2
Agustin Rodriguez <agus-socialclub@live.com>
2023-02-22 22:43:30 -0300

    Modificar formato de paginas Js interop

---
+++ SistemaTurnosOnline.Web/wwwroot/js/windowResize.js
@@ -0,0 +1,9 @@
+﻿function setWidthOfContainerFullWidth() {
+    $('article').css('max-width', '100%');
+    $('article').css('padding', '0');
+}
+
+function setWidthOfContainerContained() {
+    $('article').css('max-width', '1024px');
+    $('article').css('padding', '1.5rem');
+}
