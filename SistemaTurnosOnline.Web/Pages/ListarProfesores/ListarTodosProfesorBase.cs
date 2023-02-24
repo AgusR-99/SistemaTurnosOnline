b997f959f40e2fb2560ee7a9025536a0a8ba2837
Agustin Rodriguez <agus-socialclub@live.com>
2023-02-23 23:59:43 -0300

    Modificar listado de profesores para que no muestre al usuario logueado

--- SistemaTurnosOnline.Web/Pages/ListarProfesores/ListarTodosProfesorBase.cs
+++ SistemaTurnosOnline.Web/Pages/ListarProfesores/ListarTodosProfesorBase.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using SistemaTurnosOnline.Shared;
 using SistemaTurnosOnline.Web.Services.Contracts;
+using System.Security.Claims;
 
 namespace SistemaTurnosOnline.Web.Pages.ListarProfesores
 {
@@ -11,13 +13,23 @@ namespace SistemaTurnosOnline.Web.Pages.ListarProfesores
         public IJSRuntime Js { get; set; }
         [Inject]
         public IProfesorService ProfesorService { get; set; }
-        public IEnumerable<Profesor> Profesores { get; set; }
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationState { get; set; }
+        public List<Profesor> Profesores { get; set; }
         public List<string> Headers { get; set; } = new List<string> { "DNI", "Nombre", "Email", "" };
         [Parameter]
         public string TableId { get; set; } = "profesoresTable";
         protected override async Task OnInitializedAsync()
         {
-            Profesores = await ProfesorService.GetProfesores();
+            Profesores = (await ProfesorService.GetProfesores()).ToList();
+
+            var authState = await AuthenticationState;
+
+            var userId = authState.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+            Profesor profesorToRemove = Profesores.SingleOrDefault(profesor => profesor.Id == userId);
+
+            if (profesorToRemove != null) Profesores.Remove(profesorToRemove);
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
