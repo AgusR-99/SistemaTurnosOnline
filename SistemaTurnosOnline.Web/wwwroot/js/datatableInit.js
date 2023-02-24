c0fc3d77ba09d44672c509efeab5c1dd1e7a6454
Agustin Rodriguez <agus-socialclub@live.com>
2023-02-24 15:28:53 -0300

    Modificar propiedades de datatables

--- SistemaTurnosOnline.Web/wwwroot/js/datatableInit.js
+++ SistemaTurnosOnline.Web/wwwroot/js/datatableInit.js
@@ -1,6 +1,34 @@
 ﻿window.datatableInit = function datatableInit(table) {
     $(document).ready(function () {
-        $(table).DataTable();
+        $(table).DataTable({
+            "info": false,
+            "language": {
+                "processing": "Procesando...",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "emptyTable": "Ningún dato disponible en esta tabla",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "infoPost fix": "",
+                "search": "Buscar:",
+                "url": "",
+                "infoThousands": ",",
+                "loadingRecords": "Cargando...",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Último",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": ": Activar para ordenar la columna de manera ascendente",
+                    "sortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            }
+
+        });
     });
 }
 
