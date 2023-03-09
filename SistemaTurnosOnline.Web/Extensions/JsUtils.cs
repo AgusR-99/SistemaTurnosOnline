478c42fb039964d060ed50e49fc869b2d82b16e8
Agustin Rodriguez <agus-socialclub@live.com>
2023-03-09 00:37:46 -0300

    Implementar componente personalizado de InputText compatible con FluentValidation

---
+++ SistemaTurnosOnline.Web/Extensions/JsUtils.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace SistemaTurnosOnline.Web.Extensions
+{
+    public static class JsUtils
+    {
+        public static void SetReadOnly(this string id, IJSRuntime Js)
+        {
+            Js.InvokeVoidAsync(identifier: "setReadOnly", id);
+        }
+    }
+}
