5e30042756c52d5c92500e341449c1f3e3f09a9b
Agustin Rodriguez <agus-socialclub@live.com>
2023-03-30 17:30:51 -0300

    Agregar archivos para funcionalidad de SuccessToast

---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/SuccessToast/SuccessToast.cs
@@ -0,0 +1,11 @@
+﻿using SistemaTurnosOnline.Web.Components.CssExtensions;
+
+namespace SistemaTurnosOnline.Web.Components.ToastComponent.SuccessToast
+{
+    public class SuccessToast : ToastBase
+    {
+        public SuccessToast(string Id, string Title, string Text) : base(Id, Title, Text)
+        {
+        }
+    }
+}
---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/SuccessToast/SuccessToastBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using SistemaTurnosOnline.Web.Components.Contracts;
+using SistemaTurnosOnline.Web.Components.Contracts.ToastContracts;
+using SistemaTurnosOnline.Web.Components.CssExtensions;
+
+namespace SistemaTurnosOnline.Web.Components.ToastComponent.SuccessToast
+{
+    public class SuccessToastBase : ComponentBase, IHasId, IToastCss, IToastOptions
+    {
+        public string Icon => IconExtensions.CircleCheck;
+        public string HeaderClass => BgColorExtensions.BgSuccess;
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string Text { get; set; }
+    }
+}
