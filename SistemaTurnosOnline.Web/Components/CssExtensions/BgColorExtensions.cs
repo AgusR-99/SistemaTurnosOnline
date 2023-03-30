7d37e0e65360237ffe58dfcf054127b39a916bd0
Agustin Rodriguez <agus-socialclub@live.com>
2023-03-30 17:26:47 -0300

    Agregar clases de ayuda BgColorExtensions y IconExtensions

---
+++ SistemaTurnosOnline.Web/Components/CssExtensions/BgColorExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaTurnosOnline.Web.Components.CssExtensions
+{
+    public class BgColorExtensions
+    {
+        public const string BgSuccess = "bg-success";
+
+        public const string BgDanger = "bg-danger";
+    }
+}
---
+++ SistemaTurnosOnline.Web/Components/CssExtensions/IconExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaTurnosOnline.Web.Components.CssExtensions
+{
+    public class IconExtensions
+    {
+        public const string CircleCheck = "oi oi-circle-check";
+
+        public const string CircleX = "oi oi-circle-x";
+    }
+}
