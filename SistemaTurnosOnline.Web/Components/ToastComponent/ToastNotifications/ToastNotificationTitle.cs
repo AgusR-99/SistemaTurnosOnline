f7a71da27dd4b2686f03c887aa1c80c474892d70
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-01 20:41:02 -0300

    Agregar clases ToastNotificationTitle y GenericToastNotificationText

---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/ToastNotifications/ToastNotificationTitle.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaTurnosOnline.Web.Components.ToastComponent.ToastExtensions
+{
+    public static class ToastNotificationTitle
+    {
+        public static readonly string CreatedTitle = "Creacion exitosa";
+        public static readonly string UpdatedTitle = "Actualizacion exitosa";
+        public static readonly string ServerErrorTitle = "Error de servidor";
+    }
+}
