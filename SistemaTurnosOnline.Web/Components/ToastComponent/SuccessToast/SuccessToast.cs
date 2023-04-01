ba9b5e3dc53091d8217183084abcb229d3666c75
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-01 20:11:44 -0300

    Remover constructores de clase ToastBase.cs y convertir a clase abstracta. Agregar clase abstracta Toast.cs, la cual es heredada por SuccessToast.cs

--- SistemaTurnosOnline.Web/Components/ToastComponent/SuccessToast/SuccessToast.cs
+++ SistemaTurnosOnline.Web/Components/ToastComponent/SuccessToast/SuccessToast.cs
@@ -2,7 +2,7 @@
 
 namespace SistemaTurnosOnline.Web.Components.ToastComponent.SuccessToast
 {
-    public class SuccessToast : ToastBase
+    public class SuccessToast : Toast
     {
         public SuccessToast(string Id, string Title, string Text) : base(Id, Title, Text)
         {
--- SistemaTurnosOnline.Web/Components/ToastComponent/Toast.cs
+++ SistemaTurnosOnline.Web/Components/ToastComponent/Toast.cs
@@ -1,27 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using SistemaTurnosOnline.Web.Components.Contracts;
 using SistemaTurnosOnline.Web.Components.Contracts.ToastContracts;
+using SistemaTurnosOnline.Web.Components.Contracts;
 using SistemaTurnosOnline.Web.Extensions;
 
 namespace SistemaTurnosOnline.Web.Components.ToastComponent
 {
-    public abstract class ToastBase : ComponentBase, IHasId, IToastCss, IToastOptions, IShowable
+    public abstract class Toast : IHasId, IToastCss, IToastOptions, IShowable
     {
+        public Toast(string id, string title, string text)
+        {
+            Id = id;
+            Title = title;
+            Text = text;
+        }
+
         // The ID of the toast element.
-        [Parameter] public string Id { get; set; }
+        public string Id { get; set; }
 
         // The icon to use for the toast.
-        [Parameter] public string Icon { get; set; }
+        public string Icon { get; set; }
 
         // The CSS class to use for the header of the toast.
-        [Parameter] public string HeaderClass { get; set; }
+        public string HeaderClass { get; set; }
 
         // The title to display in the toast.
-        [Parameter] public string Title { get; set; }
+        public string Title { get; set; }
 
         // The text to display in the body of the toast.
-        [Parameter] public string Text { get; set; }
+        public string Text { get; set; }
 
         // Shows the toast using the specified JS runtime.
         public async Task Show(IJSRuntime jsRuntime)
