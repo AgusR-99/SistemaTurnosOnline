730fbeaf9294b50f4170393464425e07ea0edd68
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-01 20:13:27 -0300

    Agregar funcionalidad para DangerToast

---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/DangerToast/DangerToast.cs
@@ -0,0 +1,11 @@
+﻿using SistemaTurnosOnline.Web.Components.CssExtensions;
+
+namespace SistemaTurnosOnline.Web.Components.ToastComponent.DangerToast
+{
+    public class DangerToast : Toast
+    {
+        public DangerToast(string Id, string Title, string Text) : base(Id, Title, Text)
+        {
+        }
+    }
+}
---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/DangerToast/DangerToastBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using SistemaTurnosOnline.Web.Components.Contracts;
+using SistemaTurnosOnline.Web.Components.Contracts.ToastContracts;
+using SistemaTurnosOnline.Web.Components.CssExtensions;
+
+namespace SistemaTurnosOnline.Web.Components.ToastComponent.DangerToast
+{
+    public class DangerToastBase : ComponentBase, IHasId, IToastCss, IToastOptions
+    {
+        public string Icon => IconExtensions.CircleX;
+        public string HeaderClass => BgColorExtensions.BgDanger;
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string Text { get; set; }
+    }
+}
