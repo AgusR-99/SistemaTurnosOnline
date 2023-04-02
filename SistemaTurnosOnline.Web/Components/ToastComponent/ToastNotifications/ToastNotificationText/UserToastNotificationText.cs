6878571ea3c663d87e99e7f344a400cd4f6cf6c9
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-02 15:48:45 -0300

    Crear clase estatica UserToastNotificationText

---
+++ SistemaTurnosOnline.Web/Components/ToastComponent/ToastNotifications/ToastNotificationText/UserToastNotificationText.cs
@@ -0,0 +1,7 @@
+﻿namespace SistemaTurnosOnline.Web.Components.ToastComponent.ToastNotifications.ToastNotificationText
+{
+    public static class UserToastNotificationText
+    {
+        public static readonly string Activated = "Se ha activado al usuario correctamente";
+    }
+}
