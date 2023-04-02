d2e70e64cfeda6c0bf0c727eaaea3f18ddc4d133
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-02 15:47:57 -0300

    Agregar campo ActivatedTitle

--- SistemaTurnosOnline.Web/Components/ToastComponent/ToastNotifications/ToastNotificationTitle.cs
+++ SistemaTurnosOnline.Web/Components/ToastComponent/ToastNotifications/ToastNotificationTitle.cs
@@ -4,6 +4,7 @@
     {
         public static readonly string CreatedTitle = "Creacion exitosa";
         public static readonly string UpdatedTitle = "Actualizacion exitosa";
+        public static readonly string ActivatedTitle = "Activacion exitosa";
         public static readonly string ServerErrorTitle = "Error de servidor";
     }
 }
