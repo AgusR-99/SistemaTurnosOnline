9c625f51d0b2d668f341857c6dc17982be43dd7e
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-02 15:49:38 -0300

    Crear clase estatica ErrorHandlingUtils

---
+++ SistemaTurnosOnline.Web/Utils/ErrorHandlingUtils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using SistemaTurnosOnline.Web.Components.ToastComponent.DangerToast;
+
+namespace SistemaTurnosOnline.Web.Utils
+{
+    public static class ErrorHandlingUtils
+    {
+        public static async Task ShowServerErrorToast(Exception ex, IJSRuntime js, DangerToast serverErrorToast)
+        {
+            try
+            {
+                serverErrorToast.Text = ex.Message;
+                await serverErrorToast.Show(js);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error displaying server error toast: {e.Message}");
+            }
+        }
+    }
+}
