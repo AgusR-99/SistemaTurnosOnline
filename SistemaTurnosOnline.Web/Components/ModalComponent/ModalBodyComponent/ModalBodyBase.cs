b5120faeaede9109f97c913ba79c050fb928b549
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-05 17:40:47 -0300

    Agregar componentes ModalBody y ModalFooter

---
+++ SistemaTurnosOnline.Web/Components/ModalComponent/ModalBodyComponent/ModalBodyBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SistemaTurnosOnline.Web.Components.ModalComponent.ModalBodyComponent
+{
+    public abstract class ModalBodyBase : ComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ SistemaTurnosOnline.Web/Components/ModalComponent/ModalFooterComponent/ModalFooterBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SistemaTurnosOnline.Web.Components.ModalComponent.ModalFooterComponent
+{
+    public abstract class ModalFooterBase : ComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
