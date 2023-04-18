3e40263533059e49de3323be8eb56b98bf8ace46
Agustin Rodriguez <agus-socialclub@live.com>
2023-04-18 15:45:53 -0300

    Agregar clase estatica ToastFactory

---
+++ SistemaTurnosOnline.Web/Utils/ToastFactory.cs
@@ -0,0 +1,20 @@
+﻿using SistemaTurnosOnline.Web.Components.ToastComponent.Parent;
+using SistemaTurnosOnline.Web.Components.ToastComponent.ToastNotifications.ToastNotificationText;
+using SistemaTurnosOnline.Web.Components.ToastComponent.ToastNotifications;
+
+namespace SistemaTurnosOnline.Web.Utils
+{
+    public static class ToastFactory
+    {
+        public static ToastModel CreateServerErrorToast()
+        {
+            return new ToastModel(
+                id: "server-error-toast",
+                text: GenericToastNotificationText.ServerErrorText,
+                title: ToastNotificationTitle.ServerErrorTitle
+            );
+        }
+
+
+    }
+}
