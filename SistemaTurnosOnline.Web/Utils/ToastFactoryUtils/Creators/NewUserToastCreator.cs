652acf1abe7c70964e738149c9483e32c83bc555
Agustin Rodriguez <agus-socialclub@live.com>
2023-06-04 23:52:19 -0300

    Agregar clases creator y factory

---
+++ SistemaTurnosOnline.Web/Utils/ToastFactoryUtils/Creators/NewUserToastCreator.cs
@@ -0,0 +1,18 @@
+﻿using SistemaTurnosOnline.Web.Components.ToastComponent.Parent;
+using SistemaTurnosOnline.Web.Components.ToastComponent.ToastNotifications.ToastNotificationText;
+using SistemaTurnosOnline.Web.Components.ToastComponent.ToastNotifications;
+
+namespace SistemaTurnosOnline.Web.Utils.ToastFactoryUtils.Creators
+{
+    public class NewUserToastCreator : ToastCreator
+    {
+        public override ToastModel CreateToast()
+        {
+            return new ToastModel(
+                id: "new-user-toast",
+                title: ToastNotificationTitle.IncomingNotificationTitle,
+                text: SignalRToastNotificationText.NewUser
+            );
+        }
+    }
+}
---
+++ SistemaTurnosOnline.Web/Utils/ToastFactoryUtils/ToastCreator.cs
@@ -0,0 +1,9 @@
+﻿using SistemaTurnosOnline.Web.Components.ToastComponent.Parent;
+
+namespace SistemaTurnosOnline.Web.Utils.ToastFactoryUtils
+{
+    public abstract class ToastCreator
+    {
+        public abstract ToastModel CreateToast();
+    }
+}
