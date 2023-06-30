716747b5ead8907b7b6282a14885572184658b6d
Rodriguez I Agustin <76019584+AgusR-99@users.noreply.github.com>
2023-06-29 23:35:10 -0300

    Agregar componente para forms de edicion de datos

---
+++ SistemaTurnosOnline.Web/Components/FormComponent/FormDataEditionBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SistemaTurnosOnline.Web.Components.FormComponent
+{
+    public abstract class FormDataEditionBase : ComponentBase
+    {
+        [Parameter] public string Href { get; set; }
+        [Parameter] public string Icon { get; set; }
+        [Parameter] public string Header { get; set; }
+        [Parameter] public string HeaderSecondary { get; set; }
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+    }
+}
