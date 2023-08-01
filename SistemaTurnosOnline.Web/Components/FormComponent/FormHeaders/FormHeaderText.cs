f9d3e6c553d63338af4eeb3615801442b93b8b39
Rodriguez I Agustin <76019584+AgusR-99@users.noreply.github.com>
2023-08-01 13:15:24 -0300

    FormComponent various updates

--- SistemaTurnosOnline.Web/Components/FormComponent/FormHeaders/FormHeaderText.cs
+++ SistemaTurnosOnline.Web/Components/FormComponent/FormHeaders/FormHeaderText.cs
@@ -7,5 +7,11 @@
         public const string Security = "Seguridad";
 
         public const string Update = "Actualizar datos";
+
+        public const string UpdateTurn = "Actualizar turno";
+
+        public const string UpdateUser = "Actualizar usuario";
+
+        public const string UpdateCareer = "Actualizar carrera";
     }
 }
